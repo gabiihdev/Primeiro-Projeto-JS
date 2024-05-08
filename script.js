276095cfd68a27ffeac15788e6b3b3dce654b2d7
Gabriela Oinhos <gabiih.dev@gmail.com>
2024-05-08 15:27:25 -0300

    Foi adicionado o cálculo automático da idade

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//input
 anoNascimento = 2006
-
-idade = 2024 - anoNascimento
 nome = "Gabriela"
+
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
 mensagem = "Sou " + nome + " e tenho " + idade + " anos."
 
+//output
 alert(mensagem)
